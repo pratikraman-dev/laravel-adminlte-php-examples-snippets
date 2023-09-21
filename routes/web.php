485b5aa427b986435b0bcbd706074b26cb2d953d
devopsteam12 <developer@triforce-inc.com>
2023-09-21 12:08:12 +0530

    Update Admin Lte Plugins And Libraries

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
